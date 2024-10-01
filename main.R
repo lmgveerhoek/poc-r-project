7cfdafea9f8fe47d083023c9f4577ae096646d65
Max Veerhoek <lmg@veerhoek.eu>
2024-10-01 14:40:17 +0200

    First Commit

---
+++ main.R
@@ -0,0 +1,20 @@
+library(mlflow)
+library(dotenv)
+
+load_dot_env(file = ".env")
+
+# Function: Retrieves the MLFlow URI from the environment
+# within a local scope to reduce the risk of exposing the URI
+set_mlflow_tracking_uri <- function() {
+  # Retrieve the MLFlow URI from the environment
+  tracking_uri <- Sys.getenv("MLFLOW_TRACKING_URI")
+  cat("MLflow tracking uri set to:", tracking_uri, "\n")
+
+  # Set the MLFlow URI
+  client <- mlflow_client(tracking_uri)
+
+  # Return the client
+  return(client)
+}
+
+set_mlflow_tracking_uri()
